61400cd2fe72bf25686857c4afb0d094c35cc3f6
noah kornberg <noahkornberg@gmail.com>
2022-10-18 18:33:28 -0400

    added second page
    added nav bar and side bar

---
+++ First/log.js
@@ -0,0 +1,11 @@
+function side_close(){
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("menu").style.display = "block";
+
+}
+
+function side_open(){
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("menu").style.display = "none";
+
+}

2c254f727bb897a8a8669651ca3a3bf80a1227ab
Noah Kornberg <noahkornberg@gmail.com>
2022-11-06 18:41:52 -0500

    readded responsive sidebar

--- public/JS/log.js
+++ public/JS/log.js
@@ -1,9 +1,13 @@
-function side_close(){
-    document.getElementById("mySidebar").style.display = "none";
+function side_close() {
+    document.getElementById("mySidebar").style.width = "0%";
     document.getElementById("menu").style.display = "block";
+    document.getElementsByClassName("main")[0].style.width = "100%";
+
 }
 
-function side_open(){
-    document.getElementById("mySidebar").style.display = "block";
+function side_open() {
+    document.getElementById("mySidebar").style.width = "18%";
     document.getElementById("menu").style.display = "none";
+    document.getElementsByClassName("main")[0].style.width = "85%";
+
 }
